a232c23572c69550bdfe33a3437622c5521f1dea
berkaniZ <zohaer.berkani@gmail.com>
2026-04-07 21:25:31 +0200

    feat: première route GET / sur le serveur express

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Serveur démarré sur le port 3000"); 
-}) 
+app.listen(8080, () => {
+    console.log("Serveur démarré sur le port 8080"); 
+})
+
+app.get("/", (req, res) => {
+    res.send("Bienvenue sur Rétro Gaming Manager");
+})
